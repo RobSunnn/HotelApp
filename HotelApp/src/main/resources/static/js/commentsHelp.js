e4514cb83bf1698184a9895bfdde31675569cef8
Rob$rt0 <RobertoSpasovrs@gmail.com>
2024-05-21 23:38:34 +0300

    added some styles.....

--- HotelApp/src/main/resources/static/js/commentsHelp.js
+++ HotelApp/src/main/resources/static/js/commentsHelp.js
@@ -2,9 +2,11 @@ function showFullComment() {
     const select = document.getElementById('commentDropdown');
     const selectedOption = select.options[select.selectedIndex];
     const selectedOptionId = select.value;
+
     document.getElementById('fullCommentText').innerText = selectedOption.getAttribute('data-full-text');
 
     // Set the selected comment ID to the hidden input fields
+
     document.getElementById("selectedCommentId").value = selectedOptionId;
     document.getElementById('notApprovedCommentId').value = selectedOptionId;
 }
@@ -12,6 +14,7 @@ function showFullComment() {
 // Attach the event listener to the dropdown after the DOM content is loaded
 document.addEventListener('DOMContentLoaded', (event) => {
     const select = document.getElementById('commentDropdown');
+
     if (select) {
         select.addEventListener('change', showFullComment);
     }

019e7052272ecf8a717a499967b878936ba249b4
Robsunnn <robertospasors@gmail.com>
2024-06-11 18:09:26 +0300

    working on front end of the app...

--- HotelApp/src/main/resources/static/js/commentsAndContactRequestsScript.js
+++ HotelApp/src/main/resources/static/js/commentsAndContactRequestsScript.js
@@ -1,28 +1,70 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let rows = document.querySelectorAll("#commentTable tbody tr");
+    let rows = document.querySelectorAll("#table tbody tr");
+    let fullMessageElement = document.getElementById("fullMessage");
 
     function selectRow(row) {
         // Remove 'selected' class from any previously selected row
-        let previouslySelected = document.querySelector("#commentTable tr.selected");
+        let previouslySelected = document.querySelector("#table tr.selected");
         if (previouslySelected) {
             previouslySelected.classList.remove("selected");
         }
+        fullMessageElement.innerText = '';
 
         // Add 'selected' class to the clicked row
         row.classList.add("selected");
 
-        let fullText = row.getAttribute("data-full-text");
-        document.getElementById("fullMessage").innerText = fullText;
+        const authorName = row.getAttribute("data-content-author");
+        const authorEmail = row.getAttribute("data-content-email");
+        const authorPhoneNumber = row.getAttribute("data-content-phone");
+        const fullMessage = row.getAttribute("data-full-text");
 
-        // Retrieve the commentId from the selected row
-        let commentId = row.getAttribute("data-content-id");
+        const authorNameHeading = document.createElement('h5');
+        authorNameHeading.textContent = 'Author Name:';
+        const authorNameParagraphElement = document.createElement('p');
+        authorNameParagraphElement.textContent = authorName;
 
-        // Update the hidden input fields with the selected commentId
-        document.getElementById("selectedContentId").value = commentId;
+        const messageHeading = document.createElement('h5');
+        messageHeading.textContent = 'Message Content:';
+        const messageParagraphElement = document.createElement('p');
+        messageParagraphElement.textContent = fullMessage;
+
+        if (authorEmail) {
+            const emailHeading = document.createElement('h5');
+            emailHeading.textContent = 'Email:';
+            const emailParagraphElement = document.createElement('p');
+            emailParagraphElement.textContent = authorEmail;
+
+            const phoneNumberHeading = document.createElement('h5');
+            phoneNumberHeading.textContent = 'Phone Number:';
+            const phoneParagraphElement = document.createElement('p');
+
+            if (authorPhoneNumber) {
+                phoneParagraphElement.textContent = authorPhoneNumber;
+            } else {
+                phoneParagraphElement.textContent = `No phone number provided.`;
+            }
+            fullMessageElement.appendChild(emailHeading);
+            fullMessageElement.appendChild(emailParagraphElement);
+            fullMessageElement.appendChild(phoneNumberHeading);
+            fullMessageElement.appendChild(phoneParagraphElement);
+        }
+
+        fullMessageElement.appendChild(authorNameHeading);
+        fullMessageElement.appendChild(authorNameParagraphElement);
+        fullMessageElement.appendChild(messageHeading);
+        fullMessageElement.appendChild(messageParagraphElement);
+
+        // Retrieve the idOfElement from the selected row
+        let idOfElement = row.getAttribute("data-content-id");
+
+        // Update the hidden input fields with the selected idOfElement
+        document.getElementById("selectedContentId").value = idOfElement;
+
+        // This is for the comments table
         let notApprovedCommentInput = document.getElementById("notApprovedCommentId");
 
         if (notApprovedCommentInput) {
-            notApprovedCommentInput.value = commentId;
+            notApprovedCommentInput.value = idOfElement;
         }
     }
 
